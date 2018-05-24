0f4f2ec6535f629ff63e9cacc7d32be6e8dbe43e
decoejz <decoejz@gmail.com>
2018-05-24 16:18:01 -0300

    beleza

--- scripts/drone_drive.py
+++ scripts/drone_drive.py
@@ -6,14 +6,19 @@ from geometry_msgs.msg import Twist, Vector3
 import smach
 import smach_ros
 
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, UInt8
 import time
 
 take_off = None
 landing = None
 empty_msg = Empty()
 
+flipping = None
+
 velocidade_teste = Twist()
+
+antes = None
+t = 0
 
 class Takeoff(smach.State):
 	def __init__(self):
@@ -22,6 +27,7 @@ class Takeoff(smach.State):
 	def execute(self, userdata):
 		global take_off
 		global empty_msg
+		global antes
 
 		rospy.sleep(1.)
 		take_off.publish(empty_msg)
@@ -29,30 +35,41 @@ class Takeoff(smach.State):
 
 		print("takeoff")
 
+		antes = time.clock()
+		t = 0
 		return 'andar'
 
 class Andar(smach.State):
 	def __init__(self):
     	#alinhou1 = referente ao objeto1
-		smach.State.__init__(self, outcomes=['pousar'])
+		smach.State.__init__(self, outcomes=['pousar','andar'])
 
 	def execute(self, userdata):
 		global velocidade_saida
 		global velocidade_teste
+		global t
+		global flipping
 
-		rospy.sleep(1.)
-		vel = Twist(Vector3(0, 0, 0), Vector3(0, 0, 10))
+		rospy.sleep(0.1)
+		vel = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
 		velocidade_saida.publish(vel)
+		# rospy.sleep(0.05)
 
-		antes = time.clock()
-		depois = time.clock()
-		tempo = depois - antes
+		# depois = time.clock()
+		# tempo = depois - antes
 
-		while tempo <= 7:
-			depois = time.clock()
-			tempo = depois - antes
-		print('anda')
-		return 'pousar'
+		# if tempo <= 1:
+		print(t)
+		if t <= 25:
+			# depois = time.clock()
+			print('anda')
+			t += 1
+			return 'andar'
+		else:
+			rospy.sleep(1.)
+			flipping.publish(2)
+			rospy.sleep(2.)
+			return 'pousar'
 
 class Land(smach.State):
 	def __init__(self):
@@ -65,6 +82,7 @@ class Land(smach.State):
 		landing.publish(empty_msg)
 		print('pousa')
 		rospy.sleep(1.)
+		t = 0
 		return 'parar'
 
 # main
@@ -72,6 +90,7 @@ def main():
 	global velocidade_saida
 	global take_off
 	global landing
+	global flipping
 
 	global buffer
 
@@ -86,6 +105,8 @@ def main():
 	take_off = rospy.Publisher('bebop/takeoff', Empty, queue_size = 1)
 	landing = rospy.Publisher('bebop/land', Empty, queue_size = 1)
 	
+	flipping = rospy.Publisher('bebop/flip', UInt8, queue_size = 1)
+
     # Create a SMACH state machine
 	sm = smach.StateMachine(outcomes=['fim'])
 
@@ -96,7 +117,7 @@ def main():
 	    	transitions={'andar':'ANDAR'})
 
 		smach.StateMachine.add('ANDAR', Andar(),
-	    	transitions={'pousar':'LAND'})
+	    	transitions={'pousar':'LAND', 'andar':'ANDAR'})
 
 		smach.StateMachine.add('LAND', Land(),
 	    	transitions={'parar':'fim'})
