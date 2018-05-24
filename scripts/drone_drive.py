9c98ff8fcd25fa049300be0953bc112c771eec54
decoejz <decoejz@gmail.com>
2018-05-24 18:43:39 -0300

    precisa testar

--- scripts/drone_drive.py
+++ scripts/drone_drive.py
@@ -7,18 +7,73 @@ import smach
 import smach_ros
 
 from std_msgs.msg import Empty, UInt8
-import time
+import math
+
+# from sensor_msgs.msg import Image #, CompressedImage
+
+################################################
+# from cv_bridge import CvBridge, CvBridgeError
+# import numpy as np
+# import tf
+# import cv2 
+# import time
+# import encontra_objeto
+
+# bridge = CvBridge()
+
+# cv_image = None
+
+# # Variáveis para permitir que o roda_todo_frame troque dados com a máquina de estados
+# media = []
+# centro = []
+# area = 0.0
+
+# # tolerancia_x = 20
+# # tolerancia_y = 20
+# # ang_speed = 0.1
+# # area_ideal = 60000 # área da distancia ideal do contorno - note que varia com a resolução da câmera
+# # tolerancia_area = 20000
+
+# # Atraso máximo permitido entre a imagem sair do Turbletbot3 e chegar no laptop do aluno
+# atraso = 0.3E9
+# check_delay = True # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados
+
+# delay = 0.04
+
+# def roda_todo_frame(imagem):
+# 	print("frame")
+# 	global cv_image
+# 	global media
+# 	global centro
+# 	global area
+
+# 	now = rospy.get_rostime()
+# 	imgtime = imagem.header.stamp
+# 	lag = now-imgtime
+# 	delay = lag.secs
+# 	if delay > atraso and check_delay==True:
+# 		return 
+# 	try:
+# 		antes = time.clock()
+# 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
+
+# 		media, centro, area = encontra_objeto.identifica_cor(cv_image)
+		
+# 		depois = time.clock()
+# 		cv2.imshow("Camera", cv_image)
+# 	except CvBridgeError as e:
+# 		print('ex', e)
+################################################
 
 take_off = None
 landing = None
+flipping = None
 empty_msg = Empty()
 
-flipping = None
+#Variáveis para fazer o drone andar em espiral
+teta = None
+incremento_angular = 0.1
 
-velocidade_teste = Twist()
-
-antes = None
-t = 0
 
 class Takeoff(smach.State):
 	def __init__(self):
@@ -27,16 +82,14 @@ class Takeoff(smach.State):
 	def execute(self, userdata):
 		global take_off
 		global empty_msg
-		global antes
 
-		rospy.sleep(1.)
+		rospy.sleep(0.1)#Ver se esse tempo é OK
 		take_off.publish(empty_msg)
-		rospy.sleep(1.)
+		rospy.sleep(0.1)#Ver se esse tempo é OK
 
 		print("takeoff")
+		teta = 0
 
-		antes = time.clock()
-		t = 0
 		return 'andar'
 
 class Andar(smach.State):
@@ -46,29 +99,38 @@ class Andar(smach.State):
 
 	def execute(self, userdata):
 		global velocidade_saida
-		global velocidade_teste
-		global t
 		global flipping
+		global teta
+		global incremento_angular
+		
+		# rospy.sleep(0.1)
+		# vel = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
+		# velocidade_saida.publish(vel)
+		# rospy.sleep(0.05) #Ver se é necessário, se não der problema então deixar sem.
 
-		rospy.sleep(0.1)
-		vel = Twist(Vector3(0.5, 0, 0), Vector3(0, 0, 0))
-		velocidade_saida.publish(vel)
-		# rospy.sleep(0.05)
+		print(teta)
+		if teta <= 4*math.pi:
 
-		# depois = time.clock()
-		# tempo = depois - antes
+			x_linear = -(math.sin(teta))/2
+			y_linear = (math.cos(teta))/2
 
-		# if tempo <= 1:
-		print(t)
-		if t <= 25:
-			# depois = time.clock()
-			print('anda')
-			t += 1
+			# z_angular = incremento_angular #Usar isso para que a frente do drone também vire.
+
+			rospy.sleep(0.1)
+			vel = Twist(Vector3(x_linear, y_linear, 0), Vector3(0, 0, 0))
+			velocidade_saida.publish(vel)
+			# rospy.sleep(0.05) #Ver se é necessário, se não der problema então deixar sem.
+
+			print('rodando')
+			teta += incremento_angular
+
 			return 'andar'
+		
 		else:
-			rospy.sleep(1.)
-			flipping.publish(2)
-			rospy.sleep(2.)
+			rospy.sleep(0.5)#Ver se esse tempo é OK
+			flipping.publish(2) #0 - Forward; 1 - Backward; 2 - Right; 3 - Left
+			rospy.sleep(0.5)#Ver se esse tempo é OK
+			
 			return 'pousar'
 
 class Land(smach.State):
@@ -78,11 +140,12 @@ class Land(smach.State):
 	def execute(self, userdata):
 		global landing
 		global empty_msg
-		rospy.sleep(1.)
+		rospy.sleep(0.1)#Ver se esse tempo é OK
 		landing.publish(empty_msg)
+		
 		print('pousa')
-		rospy.sleep(1.)
-		t = 0
+		rospy.sleep(0.1)#Ver se esse tempo é OK
+		
 		return 'parar'
 
 # main
@@ -96,16 +159,15 @@ def main():
 
 	rospy.init_node('drone_drive')
 
-	# Para usar a webcam
-	# recebedor = rospy.Subscriber("raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
-
 	#Define a velocidade quando chamada.
 	velocidade_saida = rospy.Publisher("bebop/cmd_vel", Twist, queue_size = 1)
 
 	take_off = rospy.Publisher('bebop/takeoff', Empty, queue_size = 1)
 	landing = rospy.Publisher('bebop/land', Empty, queue_size = 1)
-	
 	flipping = rospy.Publisher('bebop/flip', UInt8, queue_size = 1)
+
+	#Ver para o bebop
+	# recebedor = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, roda_todo_frame, queue_size=10, buff_size = 2**24)
 
     # Create a SMACH state machine
 	sm = smach.StateMachine(outcomes=['fim'])
@@ -127,16 +189,6 @@ def main():
 
 	# Execute SMACH plan
 	outcome = sm.execute()
-
-
-	#while not rospy.is_shutdown():
-	#	take_off.publish(empty_msg)
-	#	rospy.sleep(15.)
-	#	print("takeoff")
-	#	landing.publish(empty_msg)
-	#	rospy.sleep(15.)
-	#	print('landing')
-		# outcome = sm.execute()
 
 
 if __name__ == '__main__':
